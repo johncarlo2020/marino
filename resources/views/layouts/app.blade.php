baf8fd7f5245e2582a07009300a02b2f90708dec
whei2419 <joelnacionales24@gmail.com>
2023-09-16 08:27:50 +0800

    add fb plug in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,38 @@
         <main class="mt-5">
             @yield('content')
         </main>
+          <!-- Messenger Chat Plugin Code -->
+          <div id="fb-root"></div>
+
+          <!-- Your Chat Plugin code -->
+          <div id="fb-customer-chat" class="fb-customerchat">
+          </div>
         @include('layouts.footer')
         <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
+        <script>
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "987626664960655");
+        chatbox.setAttribute("attribution", "biz_inbox");
+        </script>
+
+        <!-- Your SDK code -->
+        <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+            xfbml            : true,
+            version          : 'v18.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        </script>
     </body>
 </html>
